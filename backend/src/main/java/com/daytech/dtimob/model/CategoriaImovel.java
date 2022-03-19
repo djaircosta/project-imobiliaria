1c2cbb2b3bd33f100b2d06f2efe793645f20b029
DjairCosta <riajd10@gmail.com>
2022-03-19 11:32:55 -0300

    created classes model

--- backend/src/main/java/com/daytech/dtimob/model/CategoriaImovel.java
+++ backend/src/main/java/com/daytech/dtimob/model/CategoriaImovel.java
@@ -7,11 +7,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tipo_imovel")
-public class TipoImovel implements Serializable {
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_imvovel", sequenceName = "seq_categoria_imovel", allocationSize = 1, initialValue = 1)
+public class CategoriaImovel implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
